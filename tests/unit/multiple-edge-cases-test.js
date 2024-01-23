c99babcc887b83f3c6b3b8cf3ae9eb1648d34c42
Jacob Jewell <jacobjewell@eflexsystems.com>
2024-01-23 17:59:54 -0500

    cleanup

--- tests/unit/multiple-edge-cases-test.js
+++ tests/unit/multiple-edge-cases-test.js
@@ -1,16 +1,7 @@
-import {
-  macroCondition,
-  dependencySatisfies,
-  importSync,
-} from '@embroider/macros';
+import { expect } from 'chai';
 
-if (macroCondition(dependencySatisfies('ember-mocha', '*'))) {
-  let { describe, it } = importSync('mocha');
-  let { expect } = importSync('chai');
-
-  describe('Mocha | #3: Module With Multiple Edge Case Tests', function () {
-    it('#1 RegExp test', function () {
-      expect(/derp/.test('derp')).to.be.ok;
-    });
+describe('Mocha | #3: Module With Multiple Edge Case Tests', function () {
+  it('#1 RegExp test', function () {
+    expect(/derp/.test('derp')).to.be.ok;
   });
-}
+});
--- tests/unit/multiple-ember-tests-test.js
+++ tests/unit/multiple-ember-tests-test.js
@@ -1,43 +1,34 @@
-import {
-  macroCondition,
-  dependencySatisfies,
-  importSync,
-} from '@embroider/macros';
+import { expect } from 'chai';
+import { setupTest } from '@eflexsystems/ember-mocha';
 
-if (macroCondition(dependencySatisfies('ember-mocha', '*'))) {
-  let { describe, it } = importSync('mocha');
-  let { expect } = importSync('chai');
-  let { setupTest } = importSync('ember-mocha');
+describe('Mocha | #1: Module-For With Multiple Tests', function () {
+  setupTest();
 
-  describe('Mocha | #1: Module-For With Multiple Tests', function () {
-    setupTest();
-
-    it('#1', function () {
-      expect(true).to.be.ok;
-    });
-    it('#2', function () {
-      expect(true).to.be.ok;
-    });
-    it('#3', function () {
-      expect(true).to.be.ok;
-    });
-    it('#4', function () {
-      expect(true).to.be.ok;
-    });
-    it('#5', function () {
-      expect(true).to.be.ok;
-    });
-    it('#6', function () {
-      expect(true).to.be.ok;
-    });
-    it('#7', function () {
-      expect(true).to.be.ok;
-    });
-    it('#8', function () {
-      expect(true).to.be.ok;
-    });
-    it('#9', function () {
-      expect(true).to.be.ok;
-    });
+  it('#1', function () {
+    expect(true).to.be.ok;
   });
-}
+  it('#2', function () {
+    expect(true).to.be.ok;
+  });
+  it('#3', function () {
+    expect(true).to.be.ok;
+  });
+  it('#4', function () {
+    expect(true).to.be.ok;
+  });
+  it('#5', function () {
+    expect(true).to.be.ok;
+  });
+  it('#6', function () {
+    expect(true).to.be.ok;
+  });
+  it('#7', function () {
+    expect(true).to.be.ok;
+  });
+  it('#8', function () {
+    expect(true).to.be.ok;
+  });
+  it('#9', function () {
+    expect(true).to.be.ok;
+  });
+});
