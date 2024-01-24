7a078772e6d679f109495f55bdf36cc67b80210d
Jacob Jewell <jacobjewell@eflexsystems.com>
2024-01-23 19:03:12 -0500

    fix

--- node-tests/fixtures/test-helper-with-load.js
+++ node-tests/fixtures/test-helper-with-load.js
@@ -1,6 +1,6 @@
 import { setResolver } from '@ember/test-helpers';
 import resolver from './helpers/resolver';
-import loadEmberExam from 'ember-exam/test-support/load';
+import loadEmberExam from '@eflexsystems/ember-exam/test-support/load';
 import { start } from '@eflexsystems/ember-mocha';
 
 setResolver(resolver);
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,6 @@
 import { setResolver } from '@ember/test-helpers';
 import resolver from './helpers/resolver';
-import start from 'ember-exam/test-support/start';
+import start from '@eflexsystems/ember-exam/test-support/start';
 
 setResolver(resolver);
 start();
