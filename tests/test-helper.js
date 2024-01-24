d379ce4f6dfda1ec22e7e01469d498bdfac4900c
Jacob Jewell <jacobjewell@eflexsystems.com>
2024-01-23 19:30:44 -0500

    some tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,5 +2,7 @@ import { setResolver } from '@ember/test-helpers';
 import resolver from './helpers/resolver';
 import start from '@eflexsystems/ember-exam/test-support/start';
 
+window.mocha.setup('bdd');
+
 setResolver(resolver);
 start();
